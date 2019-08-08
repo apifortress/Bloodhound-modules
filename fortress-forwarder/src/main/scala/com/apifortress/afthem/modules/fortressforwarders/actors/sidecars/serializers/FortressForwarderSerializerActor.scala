7d2fc42ef69e4cbf3e3df4c43d057ac316718a90
theirish81 <theirish81@gmail.com>
2019-08-08 17:46:32 +0200

    removed unused import. Licence header

--- fortress-forwarder/src/main/scala/com/apifortress/afthem/modules/fortressforwarders/actors/sidecars/serializers/FortressForwarderSerializerActor.scala
+++ fortress-forwarder/src/main/scala/com/apifortress/afthem/modules/fortressforwarders/actors/sidecars/serializers/FortressForwarderSerializerActor.scala
@@ -1,10 +1,25 @@
+/**
+  * Copyright 2019 API Fortress
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  * @author Simone Pezzano
+  */
 package com.apifortress.afthem.modules.fortressforwarders.actors.sidecars.serializers
 
 import com.apifortress.afthem.actors.AbstractAfthemActor
 import com.apifortress.afthem.config.Phase
 import com.apifortress.afthem.messages.WebParsedResponseMessage
 import com.apifortress.afthem.{AfthemResponseSerializer, Parsers}
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
@@ -38,8 +53,6 @@ class FortressForwarderSerializerActor(phaseId : String) extends AbstractAfthemA
     * The HTTP client
     */
   val httpClient : HttpClient = HttpClients.createDefault()
-
-  val objectMapper : ObjectMapper = new ObjectMapper()
 
   /**
     * The URL the outbound request should be sent to
