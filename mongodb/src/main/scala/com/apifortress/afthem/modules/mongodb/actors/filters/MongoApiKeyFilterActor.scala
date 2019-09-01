7da6aab7decc73080f91d034426eb07100fa8fab
theirish81 <theirish81@gmail.com>
2019-09-01 17:22:23 +0200

    documentation

--- mongodb/src/main/scala/com/apifortress/afthem/modules/mongodb/actors/filters/MongoApiKeyFilterActor.scala
+++ mongodb/src/main/scala/com/apifortress/afthem/modules/mongodb/actors/filters/MongoApiKeyFilterActor.scala
@@ -1,3 +1,19 @@
+/**
+  * Copyright 2019 API Fortress
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  * @author Simone Pezzano
+  */
 package com.apifortress.afthem.modules.mongodb.actors.filters
 
 import com.apifortress.afthem.actors.filters.ApiKeyFilterActor
@@ -11,11 +27,21 @@ import org.mongodb.scala.{MongoClient, MongoCollection}
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
+/**
+  * Actor to filter requests based on an API key. API keys are stored in MongoDB
+  * @param phaseId
+  */
 class MongoApiKeyFilterActor(phaseId : String) extends ApiKeyFilterActor(phaseId : String) {
 
 
+  /**
+    * The MongoDB client
+    */
   private var client : MongoClient = null
 
+  /**
+    * The Collection that stores the API keys
+    */
   private var collection : MongoCollection[Document] = null
 
 
