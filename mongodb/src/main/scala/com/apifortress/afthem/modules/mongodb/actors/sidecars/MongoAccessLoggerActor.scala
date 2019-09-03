b991a1b996520a4d08beb27715f68719664d60ba
theirish81 <theirish81@gmail.com>
2019-09-03 11:06:51 +0200

    supporting rejection for invalid API keys

--- mongodb/src/main/scala/com/apifortress/afthem/modules/mongodb/actors/sidecars/MongoAccessLoggerActor.scala
+++ mongodb/src/main/scala/com/apifortress/afthem/modules/mongodb/actors/sidecars/MongoAccessLoggerActor.scala
@@ -1,8 +1,9 @@
 package com.apifortress.afthem.modules.mongodb.actors.sidecars
 
 import com.apifortress.afthem.actors.AbstractAfthemActor
-import com.apifortress.afthem.config.Phase
-import com.apifortress.afthem.messages.{WebParsedRequestMessage, WebParsedResponseMessage}
+import com.apifortress.afthem.config.{ApiKey, Phase}
+import com.apifortress.afthem.exceptions.{RejectedRequestException, UnauthorizedException}
+import com.apifortress.afthem.messages.{BaseMessage, ExceptionMessage, WebParsedRequestMessage, WebParsedResponseMessage}
 import com.apifortress.afthem.modules.mongodb.actors.TMongoDBActor
 import org.bson.Document
 
@@ -21,27 +22,49 @@ class MongoAccessLoggerActor(phaseId : String) extends AbstractAfthemActor(phase
   var bufferSize : Int = -1
 
   override def receive: Receive = {
-    case msg : WebParsedRequestMessage => {
+    case msg: ExceptionMessage if(msg.exception.isInstanceOf[RejectedRequestException]) =>
+      val exception = msg.exception.asInstanceOf[RejectedRequestException]
+      val document = new Document("type","rejected")
+      document.put("remoteIP",exception.message.request.remoteIP)
+      document.put("method",exception.message.request.method)
+      document.put("url",exception.message.request.getURL())
+      insert(document)
+    case msg: ExceptionMessage if(msg.exception.isInstanceOf[UnauthorizedException]) =>
+      val exception = msg.exception.asInstanceOf[UnauthorizedException]
+      val document = new Document("type","rejected")
+      document.put("remoteIP",exception.message.request.remoteIP)
+      document.put("method",exception.message.request.method)
+      document.put("url",exception.message.request.getURL())
+      insert(document)
+    case msg : WebParsedRequestMessage =>
       initClient(getPhase(msg))
       val document = new Document("type","inbound")
       document.put("remoteIP",msg.request.remoteIP)
       document.put("method",msg.request.method)
       document.put("url",msg.request.getURL())
+      evaluateAdditionalFields(document,msg)
       insert(document)
-    }
-    case msg : WebParsedResponseMessage => {
+
+    case msg : WebParsedResponseMessage =>
       initClient(getPhase(msg))
       val document = new Document("type","outbound")
       document.put("status",msg.response.status)
       document.put("url",msg.response.getURL())
       document.put("method",msg.response.method)
+      evaluateAdditionalFields(document,msg)
       insert(document)
-    }
+
   }
 
   override def initClient(phase : Phase) = {
     super.initClient(phase)
     bufferSize = phase.getConfigInt("buffer_size",-1)
+  }
+
+  def evaluateAdditionalFields(document : Document, msg : BaseMessage) = {
+    val keyOption = msg.meta.get("key")
+    if(keyOption.isDefined)
+      document.put("app_id",keyOption.get.asInstanceOf[ApiKey].appId)
   }
 
   override def postStop(): Unit = {
