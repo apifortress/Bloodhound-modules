34a296a6793f33e6b2392dc5025d8cf834ac6a79
theirish81 <theirish81@gmail.com>
2020-02-07 18:02:06 +0100

    licenses

--- hazelcast/src/main/scala/com/apifortress/afthem/modules/hazelcast/actors/ingresses/HazelcastIngressActor.scala
+++ hazelcast/src/main/scala/com/apifortress/afthem/modules/hazelcast/actors/ingresses/HazelcastIngressActor.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 API Fortress
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Simone Pezzano
+ */
 package com.apifortress.afthem.modules.hazelcast.actors.ingresses
 
 import java.io.File
@@ -13,7 +29,7 @@ import com.apifortress.afthem.messages.{WebParsedRequestMessage, WebParsedRespon
 import com.apifortress.afthem.modules.hazelcast.messages.HazelcastTransportMessage
 import com.hazelcast.client.HazelcastClient
 import com.hazelcast.client.config.{ClientConfig, XmlClientConfigBuilder}
-import com.hazelcast.core.{HazelcastInstance, ITopic, Message, MessageListener}
+import com.hazelcast.core.{HazelcastInstance, ITopic, Message}
 
 /**
   * The Hazelcast Ingress.
